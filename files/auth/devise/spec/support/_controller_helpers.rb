2c0dd713a7ad49a9b1082de4a219bd122b6b894f
Jerome Dalbert <jerome.dalbert@gmail.com>
2026-05-02 10:38:40 -0700

    Remove Devise workaround now that issue is fixed

--- files/auth/devise/spec/support/_controller_helpers.rb
+++ files/auth/devise/spec/support/_controller_helpers.rb
@@ -1,8 +1,6 @@
 def authenticate(user = nil)
   user ||= create(:user)
 
-  # Fix while waiting for https://github.com/heartcombo/devise/issues/5705 to be fixed
-  Rails.application.routes_reloader.execute_unless_loaded
   sign_in user
 
   user
