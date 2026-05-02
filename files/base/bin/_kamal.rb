6ea87af62ddfa9f8f9f64fb2a76b48f7fc0f4de6
Jerome Dalbert <jerome.dalbert@gmail.com>
2026-05-02 10:38:40 -0700

    Don’t explicitly set ENV[‘KAMAL_DESTINATION’]
    Kamal 2.1.0 now sets this

--- files/base/bin/_kamal.rb
+++ files/base/bin/_kamal.rb
@@ -6,7 +6,6 @@ if destination.nil?
   destination = 'production'
   ARGV.push('-d', 'production')
 end
-ENV['KAMAL_DESTINATION'] = destination
 
 # Make environment variables accessible anywhere in config/deploy.yml with ENV['MY_VAR']
 require 'dotenv'
