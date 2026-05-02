d4f20b9672c64bce47b6ff017cb5e943449b4c71
Jerome Dalbert <jerome.dalbert@gmail.com>
2026-05-02 10:38:40 -0700

    Format active storage migration

--- template.rb
+++ template.rb
@@ -241,6 +241,10 @@ module Template
 
   def install_active_storage
     run 'rails active_storage:install'
+
+    migration_file = find_file('db/migrate/*_create_active_storage_tables*.rb')
+    gsub_file migration_file, 't.references', 't.belongs_to'
+    format_code migration_file
   end
 
   def configure_dotenv
