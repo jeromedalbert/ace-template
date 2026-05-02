f2573badc4ba2053e26b1d3119fcf68cd9e76ba6
Jerome Dalbert <jerome.dalbert@gmail.com>
2026-05-02 10:38:40 -0700

    Remove Gemfile comment that prevents reordering

--- files/base/_Gemfile_dev_test_gems.rb
+++ files/base/_Gemfile_dev_test_gems.rb
@@ -1,7 +1,6 @@
 gem 'dotenv-rails'
 gem 'factory_bot_rails'
 gem 'listen'
-# Use main branch while waiting for next version that has better Rails 8 support
 gem 'rspec-rails', github: 'rspec/rspec-rails', branch: 'main'
 gem 'rubocop', require: false
 gem 'rubocop-factory_bot', require: false
