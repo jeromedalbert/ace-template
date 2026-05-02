9d20c083708ef09709eae86e61fe6929921d34dd
Jerome Dalbert <jerome.dalbert@gmail.com>
2026-05-02 10:38:40 -0700

    Use evented file watcher since this template uses the listen gem

--- files/base/config/environments/_development_end.rb
+++ files/base/config/environments/_development_end.rb
@@ -1,3 +1,5 @@
+config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
 config.generators.after_generate do |files|
   system("bundle exec stree write #{files.join(' ')} &> /dev/null")
 end
